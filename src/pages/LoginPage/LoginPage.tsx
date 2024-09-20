cd92d36838da26247ebd4a1bd76646e2fd554f19
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-09-20 09:36:17 +0800

    add shadcn form component

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/LoginForm/LoginForm"
+
+const LoginPage = () => {
+
+  return (
+    <div className="login-page">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default LoginPage
